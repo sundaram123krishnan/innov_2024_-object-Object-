93417da376e10a1c6cd7f887feda36b81e4b2558
WhyAsh5114 <whyash5114@gmail.com>
2024-02-27 15:43:57 +0530

    restructured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { NavbarDemo } from "./Navbar";
+import { NavbarDemo } from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/llama/page.tsx
+++ src/app/llama/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import VideoPlayer from "@/components/video-player";
-import { NavbarDemo } from "../Navbar";
-import MyComponent from "../SampleTextGen";
+import { NavbarDemo } from "../../components/Navbar";
+import MyComponent from "../../components/SampleTextGen";
 
 //LL-dlIgQVFTLtpQ66uuFfPlW2DMMtbq317E8KmWvS3l6CQv7KB6zmBaeuVKPBfWlyrC
 
