23c0e8722313b652f181b59a3cb540f60adb2e94
Your Name here only <krishnsundaram@gmail.com>
2024-02-27 15:31:03 +0530

    installed llama and next-video

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { withNextVideo } = require("next-video/process");
 
-export default nextConfig;
+const nextConfig = {}; // Your current Next Config object
+
+module.exports = withNextVideo(nextConfig);
