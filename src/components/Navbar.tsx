fea99a4f768325c0b5026f09ff44820361089350
Your Name here only <krishnsundaram@gmail.com>
2024-02-27 22:02:19 +0530

    modifying navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,36 +13,8 @@ import { UserButton } from "./user-button/user-button";
 
 export default function Navbar() {
   return (
-    <div className="flex w-full border-b items-center p-2 gap-1">
-      <Sheet>
-        <SheetTrigger className="pl-2 pr-1">
-          <MenuIcon strokeWidth={1.5} aria-label="menu-button" />
-        </SheetTrigger>
-        <SheetContent side="left">
-          <SheetTitle className="flex items-center gap-2 mb-3">
-            <Image
-              src="/favicon.png"
-              className="invert dark:invert-0"
-              alt="logo"
-              width={40}
-              height={40}
-            />
-            <span className="text-xl font-bold">MyFit</span>
-          </SheetTitle>
-          <div className="flex flex-col items-start pl-4">
-            <Button variant="link">
-              <Link href="/exercise_splits">Exercise splits</Link>
-            </Button>
-            <Button variant="link">
-              <Link href="/mesocycles">Mesocycles</Link>
-            </Button>
-            <Button variant="link">
-              <Link href="/exercise_splits">Workouts</Link>
-            </Button>
-          </div>
-        </SheetContent>
-      </Sheet>
-      <Link href="/" className="mr-auto">
+    <div className="flex w-full border-b items-center p-4 gap-2">
+      <Link href="/" className="px-4">
         <Image
           src="/favicon.png"
           className="dark:invert invert-0 p-1"
@@ -51,6 +23,17 @@ export default function Navbar() {
           height={40}
         />
       </Link>
+
+      <Button variant="link">
+        <Link href="/" className="px-4 text-xl font-bold">
+          Home
+        </Link>
+      </Button>
+      <Button variant="link" className="mr-auto px-4">
+        <Link href="/watch_later" className="px-4 text-xl font-bold">
+          Watch later
+        </Link>
+      </Button>
       <ModeToggle />
       <UserButton />
     </div>
